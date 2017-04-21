f2e77219a871bfd24043e721f2be6cae77abc5aa
김영준 <kim0jun1990@gmail.com>
2017-04-21 21:31:11 +0900

    add another solution 005 in javascript

--- 005/005.js
+++ 005/005.js
@@ -2,6 +2,8 @@
 
 // 그러면 1 ~ 20 사이의 어떤 수로도 나누어 떨어지는 가장 작은 수는 얼마입니까?
 
+// 첫번째 방법, 최소공배수 공식 적용
+
 var l = 1;
 var maxDivisor = {}
 var c = 0;
@@ -24,3 +26,24 @@ for( key in maxDivisor){
     l = Math.pow(key,maxDivisor[key])*l
 }
 console.log(l) //==>232792560
+
+
+// 쌩 반복문, 계산시간이 심히 오래 걸린다.
+
+var minMutiple = 2;
+while(1){
+    // 카운트는 반복문을통해 약수 체크도중 실패한 숫자(시도횟수)
+    var count = 0;
+    for(var i = 2;i<=20;i++){
+        count = i
+        if(minMutiple%i != 0){
+            minMutiple++;
+            break;
+        //공약수인경우
+        }        
+    }
+    if(count == 20){
+        console.log(minMutiple) // ==> 232792560;
+        return 0;
+    }
+}
